b93d35e4609931844e36c62508d466127a08e5b9
SalvadorMartinez3 <99310940+SalvadorMartinez3@users.noreply.github.com>
2022-04-14 19:46:41 -0700

    Server

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mysql = require('mysql');
+
+app.use(cors());
+app.use(express.json());
+
+//connects to database 
+const db = mysql.createConnection({
+    user: "Java Buddy", 
+    host: "25.72.100.118",
+    password: "Java Buddies",
+    database: "forminformation",
+});
+
+//saves informations 
+app.post('/create', (req, res) => {
+
+    const first = req.body.first;
+    const last = req.body.last;
+    const phone = req.body.phone;
+    const mail = req.body.mail;
+    const message = req.body.message;
+
+
+    db.query("INSERT INTO prototypetable (FirstName, LastName, PhoneNumber, Email, Message) VALUES(?,?,?,?,?)", 
+    [first, last, phone, mail, message], 
+    (err, result) => {
+        if (err) {
+            console.log(err)
+        } else{
+            res.send("Values Inserted");
+        }
+    } 
+    );
+});
+
+
+app.listen(3002, () =>{
+    console.log("server is running on port 25.72.100.118");
+});
