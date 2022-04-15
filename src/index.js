c055a4a4e99d75696ed5ed27cf8dae2456ed2d9c
SalvadorMartinez3 <99310940+SalvadorMartinez3@users.noreply.github.com>
2022-04-14 19:49:01 -0700

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import Title from "./component/Title"
+import"./style/global.css"
+
+const reactContentRoot = document.getElementById("root")
+
+ReactDOM.render(<Title />, reactContentRoot)
